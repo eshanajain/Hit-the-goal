cf996a3247241909a1e3eea8cd6f18f5b0686d8c
eshanajain <82397896+eshanajain@users.noreply.github.com>
2021-08-25 16:30:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -49,6 +49,7 @@ function my_keydown(e)
 		document.getElementById("hd3").innerHTML="You have Hit the goal!!!";
 		document.getElementById("myCanvas").style.borderColor="red";
 	}
+
 	
 	else{
 		if(keyPressed == '38')
@@ -72,47 +73,47 @@ function my_keydown(e)
 			console.log("right");
 		}
 	}
-	
+}
 	function up()
 {
-	if(player_y >=0){
-		player_y = player_y - block_image_height;
+	if(ball_y >=0){
+		ball_y = ball_y - block_image_height;
 		console.log("block image height = "+block_image_height);
-		console.log("When Up arrow key is pressed, X = "+player_x + ", Y = "+player_y);
-		canvas.remove(player_object);
-		player_update();
+		console.log("When Up arrow key is pressed, X = "+ball_x + ", Y = "+ball_y);
+		canvas.remove(ball_obj);
+	new_image();
 	}
 }
 
 function down()
 {
-	if(player_y <=500){
-		player_y = player_y - block_image_height;
+	if(ball_y <=500){
+		ball_y = ball_y - block_image_height;
 		console.log("block image height = "+block_image_height);
-		console.log("When Up down key is pressed, X = "+player_x + ", Y = "+player_y);
-		canvas.remove(player_object);
-		player_update();
+		console.log("When  down key is pressed, X = "+ball_x + ", Y = "+ball_y);
+		canvas.remove(ball_obj);
+	new_image();
 	}
 }
 		
 function left()
 {
-	if(player_x >=0){
-		player_x = player_x - block_image_width;
+	if(ball_x >=0){
+		ball_x = ball_x - block_image_width;
 		console.log("block image width = "+block_image_width);
-		console.log("When Up left key is pressed, X = "+player_y + ", Y = "+player_x);
-		canvas.remove(player_object);
-		player_update();
+		console.log("When Up left key is pressed, X = "+ball_y + ", Y = "+ball_x);
+		canvas.remove(ball_obj);
+		new_image();
 	}
 }
 
 function right()
 {
-	if(player_y >=850){
-		player_y = player_y - block_image_height;
-		console.log("block image height = "+block_image_height);
-		console.log("When Up arrow key is pressed, X = "+player_x + ", Y = "+player_y);
-		canvas.remove(player_object);
-		player_update();
+	if(ball_x <=850){
+		ball_x = ball_x - block_image_width;
+		console.log("block image width = "+block_image_width);
+		console.log("When right arrow key is pressed, X = "+ball_x + ", Y = "+ball_y);
+		canvas.remove(ball_obj);
+		new_image();
 	}
 }

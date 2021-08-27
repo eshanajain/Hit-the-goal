97090286601b4a16c4d723ec577f80d4ad7746dc
eshanajain <82397896+eshanajain@users.noreply.github.com>
2021-08-27 16:54:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -88,7 +88,7 @@ function my_keydown(e)
 function down()
 {
 	if(ball_y <=500){
-		ball_y = ball_y - block_image_height;
+		ball_y = ball_y + block_image_height;
 		console.log("block image height = "+block_image_height);
 		console.log("When  down key is pressed, X = "+ball_x + ", Y = "+ball_y);
 		canvas.remove(ball_obj);
@@ -110,7 +110,7 @@ function left()
 function right()
 {
 	if(ball_x <=850){
-		ball_x = ball_x - block_image_width;
+		ball_x = ball_x + block_image_width;
 		console.log("block image width = "+block_image_width);
 		console.log("When right arrow key is pressed, X = "+ball_x + ", Y = "+ball_y);
 		canvas.remove(ball_obj);
